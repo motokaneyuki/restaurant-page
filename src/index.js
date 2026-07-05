e489435f7b8e1fd54fb0f6e01a1b7d34abfe9f2a
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-07-05 20:37:50 +0900

    feat: add tab-switching logic for home and menu

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import "./styles.css";
 import initialPageLoad from "./page-load.js";
 import menu from "./menu.js";
 
+const body = document.querySelector('body');
+const content = document.querySelector('#content'); 
+const nav = document.querySelector('nav');
+
+nav.addEventListener('click', (e) => {
+    if (e.target.innerText === 'Home') {
+        content.textContent = '';
+        body.classList.remove('menuImg');
+        initialPageLoad();
+    } else if (e.target.innerText === 'Menu') {
+        content.textContent = '';
+        menu();
+    }
+})
+
 initialPageLoad();
 // menu();
